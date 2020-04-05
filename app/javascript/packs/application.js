b19d0db8c07ea42f33bbb51204afe2293286ff68
Yasuhiro Usutani <y_usutani@mac.com>
2020-04-05 16:53:04 +0900

    Add Custom HTTP Header

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+document.addEventListener("turbolinks:request-start", function(event) {
+    var xhr = event.data.xhr
+    xhr.setRequestHeader("X-Request-Id", "123...")
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
